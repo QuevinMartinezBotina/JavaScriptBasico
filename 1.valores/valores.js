6eebe7016c94031d798f58a8368d4e6ff3c930e5
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2021-09-29 13:42:58 -0500

    First commit

---
+++ 1.valores/valores.js
@@ -0,0 +1,22 @@
+/* Number */
+40
+
+    /* String */
+    "40"
+
+
+/* Boolean */
+true
+false
+
+/* valores vacios */
+null
+undefined
+
+    /* Array */
+    [1, 2, 3, 4]
+
+/* Objeto */
+{
+    nombre: "Santiago"
+}
