bb1617f9ec65c6123873343b8937f73f1b7b7852
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2022-01-04 09:27:33 -0500

    webpack - enviando a production netlify

---
+++ 5.cursoProfesinalJS/2.repaso/js/Intersection-Observer/Intersection-Observer.js
@@ -0,0 +1,9 @@
+const videoHTML = document.querySelector("#video");
+const options = {
+    //root,
+    //rootMargin: 
+    //threshold: 0,
+}
+
+
+const observer = new IntersectionObserver(callback, options);
